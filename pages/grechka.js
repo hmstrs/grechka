e30eb183ddfa3689b80e76d83aa0d995c023bfb2
Rumiantsev Oleksii <rumiantsev.oleksii@gmail.com>
2021-01-24 16:35:07 +0200

    feat(front): grechka page

---
+++ pages/grechka.js
@@ -0,0 +1,28 @@
+import Chart from "../components/Chart";
+import ProductsGrid from "../components/ProductsGrid";
+import styles from "../styles/Header.module.css";
+import Link from "next/link";
+
+const Grechka = ({ grechkaPrices, grechka }) => {
+  return (
+    <>
+      <Link href="/">
+        <h1 className={styles.Header__logoGrechka}>Гречка</h1>
+      </Link>
+      <main>
+        <Chart productPrices={grechkaPrices} />
+        <ProductsGrid productsList={grechka} />
+      </main>
+    </>
+  );
+};
+
+export const getServerSideProps = async ({ res }) =>
+  ({
+    props: {
+      grechka: res.result.data ?? [],
+      grechkaPrices: res.result.statistics ?? [],
+    },
+  });
+
+export default Grechka;
