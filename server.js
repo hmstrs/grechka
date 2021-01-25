34bc81a35166f01ff71a06ad64fcc21540a2dcab
Rumiantsev Oleksii <rumiantsev.oleksii@gmail.com>
2021-01-25 17:06:13 +0200

    fixup: add hostname binding

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ fastify
 
 (async () => {
   try {
-    await fastify.listen(process.env.PORT || 3000);
+    await fastify.listen(process.env.PORT || 3000, '0.0.0.0');
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
